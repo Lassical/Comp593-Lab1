5a78134992340530d3fefee93933bbe2fce87a31
Alex <122479277+Lassical@users.noreply.github.com>
2023-01-18 13:57:04 -0500

    The Dutch return
    And Greedo shot first

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 print("There should be one-- and preferably only one --obvious way to do it.")
-# The Dutch
+#The Dutch
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("Greedo shot first")
