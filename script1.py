aa9727ed132859f47898e99eb4951d738040aa39
Alex <122479277+Lassical@users.noreply.github.com>
2023-01-18 13:42:28 -0500

    Deleted Explicit Text
    Get it? Explicit? like the line that says... never mind.

--- script1.py
+++ script1.py
@@ -1,4 +1,3 @@
 print("Beautiful is better than ugly.")
 #Beauty is in the eye of the beholder or something i dont know
-print("Explicit is better than implicit.")
 print("Simple is better than complex.")
