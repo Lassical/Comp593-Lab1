e75ddc63bf87ebc5a89e96a3a06263f30fb9c0d1
Alex <122479277+Lassical@users.noreply.github.com>
2023-01-18 14:12:23 -0500

    Threaten poeple
    For having the wrong opinion

--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@ print("There should be one-- and preferably only one --obvious way to do it.")
 #The Dutch
 print("Although that way may not be obvious at first unless you're Dutch.")
 print("Greedo shot first")
+#Fight me in the comments, I dare you
