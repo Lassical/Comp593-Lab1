f844c0219ac09ac1b6e1ca1dcd74f183408cacb8
Alex <122479277+Lassical@users.noreply.github.com>
2023-01-18 14:03:56 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,3 @@
 print("There should be one-- and preferably only one --obvious way to do it.")
 #The Dutch
 print("Although that way may not be obvious at first unless you're Dutch.")
-print("Greedo shot first")
