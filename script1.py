bf0d4e9a97038ebee43f2499e7a4952bdcc33ce5
Alex <122479277+Lassical@users.noreply.github.com>
2023-01-18 13:39:11 -0500

    Added comment to Script1
    Added a thing to the thing or something

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
 print("Beautiful is better than ugly.")
+#Beauty is in the eye of the beholder or something i dont know
 print("Explicit is better than implicit.")
 print("Simple is better than complex.")
